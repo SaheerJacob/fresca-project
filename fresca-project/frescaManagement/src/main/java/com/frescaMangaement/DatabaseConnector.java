b450bd67ba0cb5efcde9b969f63b72cb65ad6e3e
SaheerJacob <53510885+SaheerJacob@users.noreply.github.com>
2024-04-10 16:32:22 +0300

    New project

---
+++ fresca-project/frescaManagement/src/main/java/com/frescaMangaement/DatabaseConnector.java
@@ -0,0 +1,25 @@
+package com.frescaMangaement;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/frescaManagement";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Fresca";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
